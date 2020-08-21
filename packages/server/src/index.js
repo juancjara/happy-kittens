249b25bec771a6305ab99356c154f1d1c9318ecc
juancjara <juanc.jara@pucp.pe>
2020-08-20 21:06:28 -0500

    added middlewares

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -2,10 +2,18 @@ require('dotenv').config()
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+// TODO set origin url
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.json({foo: 'bar'});
