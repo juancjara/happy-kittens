3d2f8d81a8d785520cc40d93b09a8b0c44ba472e
Andre Quispesaravia <andreq@fb.com>
2021-02-15 21:24:53 -0800

    [Server] enable static resources flushing https in localhost and add instructions

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,29 +1,37 @@
-require('dotenv').config()
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const cors = require('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-
+require("dotenv").config();
+const express = require("express");
+const https = require("https");
+const socketIO = require("socket.io");
+const fs = require("fs");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const path = require("path");
+const { fstat } = require("fs");
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(
+  {
+    key: fs.readFileSync("../../localhost.key"),
+    cert: fs.readFileSync("../../localhost.crt"),
+  },
+  app
+);
 const io = socketIO(server);
 
 // TODO set origin url
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(helmet());
+app.use(express.static("../web/build"));
 
-app.get('/', (req, res) => {
-  res.json({foo: 'bar'});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../../web/build/index.html"));
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(process.env.PORT, () => {
-  console.log('listening on ', process.env.PORT);
+  console.log("listening on ", process.env.PORT);
 });
-
